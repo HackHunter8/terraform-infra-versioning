fba626fa9fb76ba4f4879ce6b2497249f8e58c34
Davidwealth8 <160414933+Davidwealth8@users.noreply.github.com>
2025-06-02 22:30:04 +0100

    feat: add VPC module and initialize backend

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-infra-versioning-state"
+    bucket         = "terraform-infra-versioning-state-david-2025"
     key            = "dev/terraform.tfstate"   # change per env
     region         = "us-east-1"
     dynamodb_table = "terraform-infra-versioning-lock"
@@ -10,8 +10,15 @@ terraform {
   }
 }
 
+provider "aws" {
+   region = "us-east-1"
+}
+
 module "vpc" {
-  source      = "../../modules/vpc"
-  cidr_block  = "10.0.0.0/16"
-  name_prefix = "dev"
+  source               = "../../modules/vpc"
+  cidr_block           = "10.0.0.0/16"
+  name_prefix          = "dev"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
+  availability_zones   = ["us-east-1a","us-east-1b"] 
 }
